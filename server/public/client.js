c19ddc8b7782f3dd7160ed2a4b4f5b2258fce16c
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-06 10:34:03 -0600

    valuesObj now captures all of the needed info for calculation

--- server/public/client.js
+++ server/public/client.js
@@ -4,19 +4,31 @@ function onReady() {
   console.log('jq');
   $(document).on('click', '#submitBtn', onSubmit);
   $(document).on('click', '#clearBtn', onClear);
-  $(document).on('click', '#');
+  // listen for operator button click
+  $(document).on('click', '.operatorBtn', onOperator);
+}
+
+let operator = '';
+
+function onOperator() {
+  let operatorClick = $(this).attr('id');
+  return (operator = operatorClick);
 }
 
 function onSubmit(evt) {
   evt = event.preventDefault();
   console.log('onSubmit');
+  console.log(operator);
 
-  let numOne = $('#numOneInput');
-  let numTwo = $('#numTwoInput');
+  let numOne = $('#numOneInput').val();
+  let numTwo = $('#numTwoInput').val();
+
   let valuesObj = {
     numOne: numOne,
     numTwo: numTwo,
+    operator: operator,
   };
+  console.log(valuesObj);
 }
 
 function onClear() {
